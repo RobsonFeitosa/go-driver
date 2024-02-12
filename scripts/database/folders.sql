8287c75e95817a747ac0d3e284671e07679be1c4
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-12 16:39:36 -0300

    fixed: sqls

--- scripts/database/folders.sql
+++ scripts/database/folders.sql
@@ -4,8 +4,8 @@ CREATE TABLE folders (
   name VARCHAR(60) NOT NULL,
   created_at TIMESTAMP DEFAULT current_timestamp,
   modified_at TIMESTAMP NOT NULL,
-  deleted bool  NOT NULL DEFAULT false 
-  PRIMARY KEY(id)
+  deleted bool  NOT NULL DEFAULT false, 
+  PRIMARY KEY(id),
   CONSTRAINT fk_parent
     FOREIGN KEY(parent_id)
       REFERENCES folders(id)
