fbd22739d88c9f022964b7bffc8dc2e1ac64299b
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-13 19:17:57 -0300

    create struct user

---
+++ users/entity.go
@@ -0,0 +1,76 @@
+package users
+
+import (
+	"crypto/md5"
+	"errors"
+	"fmt"
+	"time"
+)
+
+var (
+	ErrNameRequire = errors.New("Name is required")
+
+	ErrLoginRequire = errors.New("Login is required")
+
+	ErrPasswordRequire = errors.New("Passwords is required and can't be blank")
+
+	ErrPasswordLength = errors.New("Password must have at least 6 characters")
+)
+
+func New(name, login, password string) (*User, error) {
+	u := User{Name: name, Login: login, ModifiedAt: time.Now()}
+	err := u.SetPassword(password)
+	if err != nil {
+		return nil, err
+	}
+
+	err = u.Validate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
+
+type User struct {
+	ID         int64
+	Name       string
+	Login      string
+	Password   string
+	CreatedAt  time.Time
+	ModifiedAt time.Time
+	Deleted    bool
+	LastLogiin time.Time
+}
+
+func (u *User) SetPassword(password string) error {
+	if password == "" {
+		return ErrPasswordRequire
+	}
+
+	if len(password) < 6 {
+		return ErrPasswordLength
+	}
+
+	u.Password = fmt.Sprintf("%x", (md5.Sum([]byte(password))))
+
+	return nil
+}
+
+func (u *User) Validate() error {
+
+	if u.Name == "" {
+		return ErrNameRequire
+	}
+
+	if u.Login == "" {
+		return ErrLoginRequire
+	}
+
+	BlankPassword := fmt.Sprintf("%x", md5.Sum([]byte("")))
+	if u.Password == BlankPassword {
+		return ErrPasswordRequire
+	}
+
+	return nil
+}
