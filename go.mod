7b109700560a6ff0255e1995878560dbc659fb5d
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-13 19:17:35 -0300

    create CRUD users

--- go.mod
+++ go.mod
@@ -4,5 +4,15 @@ go 1.18
 
 require (
 	github.com/aws/aws-sdk-go v1.50.16
+	github.com/go-chi/chi v1.5.5
 	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/stretchr/testify v1.8.0
+)
+
+require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ users/get.go
@@ -0,0 +1,41 @@
+package users
+
+import (
+	"database/sql"
+	"encoding/json"
+	"net/http"
+	"strconv"
+
+	"github.com/go-chi/chi"
+)
+
+func (h *handler) GetByID(rw http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(chi.URLParam(r, "id"))
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	u, err := Get(h.db, int64(id))
+	if err != nil {
+		// TODO validar se o erro é pq nao existe nenhum registro
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Header().Add("Content-Type", "application/json")
+	json.NewEncoder(rw).Encode(u)
+}
+
+func Get(db *sql.DB, id int64) (*User, error) {
+	stmt := `SELECT * FROM "users" WHERE id=$1`
+	row := db.QueryRow(stmt, id)
+
+	var u User
+	err := row.Scan(&u.ID, &u.Login, &u.Password, &u.CreatedAt, &u.ModifiedAt, &u.Deleted, &u.LastLogin)
+	if err != nil {
+		return nil, err
+	}
+
+	return &u, nil
+}
