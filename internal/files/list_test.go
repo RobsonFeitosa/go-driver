2d7f867473bd2fce7bb6dba518784556194076fc
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 14:50:45 -0300

    test unitário para o servićo de atualizaćão do arquivo

---
+++ internal/files/list_test.go
@@ -0,0 +1,34 @@
+package files
+
+import (
+	"regexp"
+	"testing"
+	"time"
+
+	"github.com/DATA-DOG/go-sqlmock"
+)
+
+func TestList(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "modified_at", "deleted"}).
+		AddRow(1, 1, 1, "robson", "jpg", "/src", time.Now(), time.Now(), false).
+		AddRow(2, 2, 2, "ana", "jpg", "/src", time.Now(), time.Now(), false)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "files" WHERE  "folder_id" is null and "deleted"=false`)).
+		WillReturnRows(rows)
+
+	_, err = ListRoot(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
