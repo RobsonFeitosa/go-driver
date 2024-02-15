64ac8147c95517925e16ebb4fa6a0702835144f3
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:59:02 -0300

    test unitário atualizaćão de usuário

--- internal/users/modify_test.go
+++ internal/users/modify_test.go
@@ -1,11 +1,69 @@
 package users
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 )
+
+func TestModify(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	u := User{
+		ID:   1,
+		Name: "Robson",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(&u)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/{id}", &b)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE "users" SET "name"=$1, "modified_at"=%2 WHERE id=%3)`)).
+		WithArgs("Robson", AnyTime{}, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "modified_at", "deleted", "last_login"}).
+		AddRow(1, "Robson", "robson.gw@hotmail.com", "123456", time.Now(), time.Now(), false, time.Now())
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "users" WHERE id=$1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	h.Modify(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestUpdate(t *testing.T) {
 	db, mock, err := sqlmock.New()
