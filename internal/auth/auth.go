16c9464250e917cc81c6181bdeee7b5352c105ab
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-15 20:53:06 -0300

    autenticando usuário

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"encoding/json"
 	"net/http"
+	"time"
 
 	"github.com/RobsonFeitosa/go-driver/internal/users"
 	"github.com/golang-jwt/jwt/v5"
@@ -10,14 +11,26 @@ import (
 
 var jwtSecret = "698dc19d489c4e4db73e28a713eab07b"
 
-func createToken(user users.User) (string, error) {
-
-}
-
 type Claims struct {
-	UserID   string `json:"user_id"`
+	UserID   int64  `json:"user_id"`
 	UserName string `json:"user_name"`
 	jwt.RegisteredClaims
+}
+
+func createToken(user users.User) (string, error) {
+	expirationTime := time.Now().Add(30 * time.Minute)
+
+	claims := &Claims{
+		UserID:   user.ID,
+		UserName: user.Name,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+
+	return token.SignedString(jwtSecret)
 }
 
 type Credentials struct {
@@ -33,4 +46,11 @@ func Auth(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	token, err := createToken(u)
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	rw.Write([]byte(token))
 }
