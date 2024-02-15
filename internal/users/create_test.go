6b5b962bedb370f5949bee6b5f540bb818ca81b9
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:57:11 -0300

    modificando teste suite before e after

--- internal/users/create_test.go
+++ internal/users/create_test.go
@@ -1,34 +1,39 @@
 package users
 
 import (
-	"testing"
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/stretchr/testify/assert"
 )
 
-func TestInsert(t *testing.T) {
-	db, mock, err := sqlmock.New()
-	if err != nil {
-		t.Error(err)
-	}
-	defer db.Close()
+func (ts *TransactionSuite) TestCreate() {
 
-	u, err := New("Robson", "robson.gw@hotmail.com", "123456")
-	if err != nil {
-		t.Error(err)
-	}
+	var b bytes.Buffer
+	err := json.NewEncoder(&b).Encode(ts.entity)
+	assert.NoError(ts.T(), err)
 
-	mock.ExpectExec(`INSERT INTO "users" ("name", "login", "password", "modified_at")*`).
-		WithArgs("Robson", "robson.gw@hotmail.com", u.Password, u.ModifiedAt).
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPost, "/", &b)
+
+	ts.entity.SetPassword(ts.entity.Password)
+
+	ts.mock.ExpectExec(`INSERT INTO "users" ("name", "login", "password", "modified_at")*`).
+		WithArgs(ts.entity.Name, ts.entity.Login, ts.entity.Password, ts.entity.ModifiedAt).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
-	_, err = Insert(db, u)
-	if err != nil {
-		t.Error(err)
-	}
+	ts.handler.Create(rr, req)
+	assert.Equal(ts.T(), http.StatusCreated, rr.Code)
+}
 
-	err = mock.ExpectationsWereMet()
-	if err != nil {
-		t.Error(err)
-	}
+func (ts *TransactionSuite) TestInsert() {
+	ts.mock.ExpectExec(`INSERT INTO "users" ("name", "login", "password", "modified_at")*`).
+		WithArgs("Robson", "robson.gw@hotmail.com", ts.entity.Password, ts.entity.ModifiedAt).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	_, err := Insert(ts.conn, ts.entity)
+	assert.NoError(ts.T(), err)
 }
