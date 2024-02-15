9ebbcc8b9a5c81162260bb8f28d1692ef4145bca
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:49:10 -0300

    definindo teste delete http

--- internal/files/delete_test.go
+++ internal/files/delete_test.go
@@ -1,10 +1,47 @@
 package files
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 )
+
+func TestDeleteHTTP(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db, nil, nil}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/{id}", nil)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	mock.ExpectExec(`UPDATE "files" SET *`).
+		WithArgs(AnyTime{}, 1).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Delete(rr, req)
+
+	if rr.Code != http.StatusNoContent {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestDelete(t *testing.T) {
 	db, mock, err := sqlmock.New()
