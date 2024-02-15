3b0551d48ae5ca80a13677fc709deaa2d413e7b5
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:51:00 -0300

    test unitário arquivo de modificaćão

--- internal/files/modify_test.go
+++ internal/files/modify_test.go
@@ -1,11 +1,69 @@
 package files
 
 import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
+	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 )
+
+func TestModify(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db, nil, nil}
+
+	f := File{
+		ID:   1,
+		Name: "Robson.jpg",
+	}
+
+	var b bytes.Buffer
+	err = json.NewEncoder(&b).Encode(&f)
+	if err != nil {
+		t.Error(err)
+	}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodPut, "/{id}", &b)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "created_at", "modified_at", "deleted", "last_login"}).
+		AddRow(1, 1, 1, "Robson.jpg", "image/jpg", "/", time.Now(), time.Now(), false)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`select * from "files" where id = $1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE "files" SET "name"=$1, "modified_at"=$2, "deleted"=%3 WHERE id=%4`)).
+		WithArgs(f.Name, AnyTime{}, false, f.ID).
+		WillReturnResult(sqlmock.NewResult(1, 1))
+
+	h.Modify(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestUpdate(t *testing.T) {
 	db, mock, err := sqlmock.New()
@@ -14,7 +72,7 @@ func TestUpdate(t *testing.T) {
 	}
 	defer db.Close()
 
-	mock.ExpectExec(regexp.QuoteMeta(`UPDATE "files" SET "name"=$1, "modify_at"=$2, "deleted"=%3 WHERE id=%4)`)).
+	mock.ExpectExec(regexp.QuoteMeta(`UPDATE "files" SET "name"=$1, "modified_at"=$2, "deleted"=%3 WHERE id=%4`)).
 		WithArgs("Robson", AnyTime{}, false, 1).
 		WillReturnResult(sqlmock.NewResult(1, 1))
 
