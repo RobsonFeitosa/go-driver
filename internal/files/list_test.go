2e3c9bd70cd4b16131ef63a7b6e5771c6aeaf908
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:49:57 -0300

    test unitário lista origin de arquivos

--- internal/files/list_test.go
+++ internal/files/list_test.go
@@ -32,3 +32,28 @@ func TestList(t *testing.T) {
 		t.Error(err)
 	}
 }
+
+func TestListRoot(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	rows := sqlmock.NewRows([]string{"id", "folder_id", "owner_id", "name", "type", "path", "created_at", "modified_at", "deleted"}).
+		AddRow(1, nil, 1, "gopher", "image/jpg", "/", time.Now(), time.Now(), false).
+		AddRow(2, nil, 2, "logo", "image/jpg", "/", time.Now(), time.Now(), false)
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "files" WHERE  "folder_id" is null and "deleted"=false`)).
+		WillReturnRows(rows)
+
+	_, err = ListRoot(db)
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
