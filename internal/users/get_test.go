cd807720a6e2b335de6cc690091c0905920e0af5
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-14 23:58:08 -0300

    test unitário para buscar usuário

--- internal/users/get_test.go
+++ internal/users/get_test.go
@@ -1,12 +1,52 @@
 package users
 
 import (
+	"context"
+	"net/http"
+	"net/http/httptest"
 	"regexp"
 	"testing"
 	"time"
 
 	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/go-chi/chi"
 )
+
+func TestGetByID(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Error(err)
+	}
+	defer db.Close()
+
+	h := handler{db}
+
+	rr := httptest.NewRecorder()
+	req := httptest.NewRequest(http.MethodGet, "/{id}", nil)
+
+	ctx := chi.NewRouteContext()
+	ctx.URLParams.Add("id", "1")
+
+	req = req.WithContext(context.WithValue(req.Context(), chi.RouteCtxKey, ctx))
+
+	rows := sqlmock.NewRows([]string{"id", "name", "login", "password", "created_at", "modified_at", "deleted", "last_login"}).
+		AddRow(1, "Robson", "robson.gw@hotmail.com", "123456", time.Now(), time.Now(), false, time.Now())
+
+	mock.ExpectQuery(regexp.QuoteMeta(`SELECT * FROM "users" WHERE id=$1`)).
+		WithArgs(1).
+		WillReturnRows(rows)
+
+	h.GetByID(rr, req)
+
+	if rr.Code != http.StatusOK {
+		t.Errorf("Error: %v", rr)
+	}
+
+	err = mock.ExpectationsWereMet()
+	if err != nil {
+		t.Error(err)
+	}
+}
 
 func TestGet(t *testing.T) {
 	db, mock, err := sqlmock.New()
