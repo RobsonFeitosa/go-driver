cbc1314c44d91ac86811c7cfa375dd5866174d19
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-15 23:32:26 -0300

    criando auth request e arquivo token de configuraćão

--- go.mod
+++ go.mod
@@ -3,18 +3,21 @@ module github.com/RobsonFeitosa/go-driver
 go 1.18
 
 require (
+	github.com/DATA-DOG/go-sqlmock v1.5.2
 	github.com/aws/aws-sdk-go v1.50.16
 	github.com/go-chi/chi v1.5.5
+	github.com/golang-jwt/jwt/v5 v5.2.0
+	github.com/lib/pq v1.10.9
 	github.com/rabbitmq/amqp091-go v1.9.0
+	github.com/spf13/cobra v1.8.0
 	github.com/stretchr/testify v1.8.0
 )
 
 require (
-	github.com/DATA-DOG/go-sqlmock v1.5.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/golang-jwt/jwt/v5 v5.2.0 // indirect
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pkg/requests/auth.go
+++ pkg/requests/auth.go
@@ -1,5 +1,58 @@
 package requests
 
+import (
+	"bytes"
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+)
+
+type credentials struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
 func Auth(path, user, pass string) error {
+	creds := credentials{user, pass}
+
+	var body bytes.Buffer
+	err := json.NewEncoder(&body).Encode(creds)
+	if err != nil {
+		return err
+	}
+
+	resp, err := doRequest("POST", path, &body, false)
+	if err != nil {
+		return err
+	}
+
+	return createTokenCache(resp.Body)
+}
+
+type cacheToken struct {
+	Token string `json:"token"`
+}
+
+func createTokenCache(body io.ReadCloser) error {
+	token, err := ioutil.ReadAll(body)
+	if err != nil {
+		return err
+	}
+
+	file, err := os.Create(".cacheToken")
+	if err != nil {
+		return err
+	}
+
+	cache := cacheToken{string(token)}
+	data, err := json.Marshal((&cache))
+	if err != nil {
+		return err
+	}
+
+	_, err = file.Write(data)
+
+	return err
 
 }
