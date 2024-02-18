5ddce3825eeb01e708868628d7eee50303e30971
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-18 20:46:44 -0300

    Correćoes finais

--- scripts/shell/build.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-
-declare -A apps=(["api"]="api" ["drive"]="cli" ["worker"]="worker")
-
-for app in "${!apps[@]}"; do
-  echo "building $app..."
-  go build -o $app ./cmd/${apps[$app]}
-
-done 
