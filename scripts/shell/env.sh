ff4debd64b4566aa93a2d727a96af51bd228e63c
RobsonFeitosa <robson.gw@hotmail.com>
2024-02-18 21:04:28 -0300

    removendo credenciais aws

--- scripts/shell/env.sh
+++ scripts/shell/env.sh
@@ -7,10 +7,7 @@ export SERVER_PORT=8090
 export RABBIT_URL=localhost
 export RABBIT_TOPIC_NAME=upload 
 export AWS_REGION=us-east-1
-export AWS_KEY=AKIA6GBMEFLCQFZRECGK
-export AWS_SECRET=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
+export AWS_KEY=
+export AWS_SECRET=
 
-
-# export AWS_KEY=AKIA6GBMEFLC2IPA5MRH
-# export AWS_SECRET=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
  
